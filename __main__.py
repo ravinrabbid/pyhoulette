2e922db31e1198ffaadd577760293ea5912a365e
Frederik Walk <f_walk09@cs.uni-kl.de>
2013-11-26 14:49:26 +0100

    Hpgl parsing

--- __main__.py
+++ __main__.py
@@ -1,29 +1,42 @@
 import sys
+import signal
+import pysvg.parser
+
 from gi.repository import Gtk
 
-def quit():
-	print "Quitting..."
-	Gtk.main_quit()
-
 class Gui:
-
 	class Handler:
+		def __init__(self, gui):
+			self.gui = gui
+
 		def onDeleteMainWindow(self, *args):
 			quit()
 
 		def onButtonQuit(self, *args):
 			quit()
 
-	def __init__(self):
+		def onButtonOpen(self, *args):
+			self.gui.filechooser.run()
+
+		def onSelectFile(self, *args):
+			path = self.gui.filechooser.get_filename()
+			self.gui.parent.openFile(path)
+
+	def pushMessage(self, msg):
+		self.statusbar.push(0, msg)
+
+	def __init__(self, parent):
+		self.parent = parent
+
 		self.builder = Gtk.Builder()
 		self.builder.add_from_file("gtk/pyhoulette.glade")
-		self.builder.connect_signals(self.Handler())
+		self.builder.connect_signals(self.Handler(self))
 
 		self.width = self.builder.get_object("sb_width")
-		self.width_adjustment = Gtk.Adjustment(300, 1, 350, 1, 1, 0)
+		self.width_adjustment = Gtk.Adjustment(300, 1, 10000, 1, 1, 0)
 		self.width.set_adjustment(self.width_adjustment)
 		self.heigth = self.builder.get_object("sb_heigth")
-		self.heigth_adjustment = Gtk.Adjustment(300, 1, 350, 1, 1, 0)
+		self.heigth_adjustment = Gtk.Adjustment(300, 1, 10000, 1, 1, 0)
 		self.heigth.set_adjustment(self.heigth_adjustment)
 
 		self.speed = self.builder.get_object("s_speed")
@@ -35,14 +48,42 @@ class Gui:
 		self.pressure.set_adjustment(self.pressure_adjustment)
 
 		self.statusbar = self.builder.get_object("statusbar1")
-		self.statusbar.push(0, "ready!")
+		self.pushMessage("ready!")
 
 		self.window = self.builder.get_object("window1")
 
+		self.filechooser = self.builder.get_object("filechooser")
+
+class Pyhoulette:
+	def quit(self):
+		print "Quitting..."
+		Gtk.main_quit()
+
+	def parseHpgl(self, data):
+		hpgl = data.read().split(";", -1)
+		hpgl = filter(lambda x: x.startswith(("PU","PD")), hpgl)
+		movements = map(lambda x: x.replace("PU", "M"), hpgl)
+		self.movements = map(lambda x: x.replace("PD", "D"), movements)
+		print self.movements
+
+	def openFile(self, path):
+		data = open(path, "r")
+		self.parseHpgl(data)
+		self.gui.pushMessage("File loaded")
+
+		
+	def run(self):
+		self.gui.window.show_all()
+		Gtk.main()
+
+	def __init__(self, *cmd_args):
+		self.gui = Gui(self)
+		self.movements = []
+
 def main(*cmd_args):
-	gui = Gui()
-	gui.window.show_all()
-	Gtk.main()
+	instance = Pyhoulette(*cmd_args)
+	instance.run()
 
 if __name__ == '__main__':
-        main(*sys.argv[1:])
+		signal.signal(signal.SIGINT, signal.SIG_DFL)
+		main(*sys.argv[1:])
